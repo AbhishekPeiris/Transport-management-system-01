c2579b6c100572e450c460d960cde42f2619a784
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-09-25 16:07:36 +0530

    add duty

--- backend/routes/DutyRoute.js
+++ backend/routes/DutyRoute.js
@@ -2,4 +2,46 @@ const express = require('express');
 const router = express.Router();
 const Duty = require('../models/Duty');
 
+router.route('/addduty').post(async (req, res) => {
+
+    const {
+        employeeId,
+        dutyDate,
+        vehicleId,
+        startLocation,
+        endLocation,
+        distance,
+        dutyStatus,
+        shift,
+        notes,
+    } = req.body;
+
+    const newDuty = new Duty({
+
+        employeeId,
+        dutyDate,
+        vehicleId,
+        startLocation,
+        endLocation,
+        distance,
+        dutyStatus,
+        shift,
+        notes,
+
+    });
+
+    try {
+
+        await newDuty.save();
+        return res.status(200).json({ status: "Duty is added successfully" });
+
+    } catch (error) {
+
+        return res.status(500).json({ status: "Error with adding duty", messsage: error });
+    }
+});
+
+
+
+
 module.exports = router;
